020b9ed4930559334da80bbc5b587872c1ccb6c6
Eduardo <emoburrito_copy@hotmail.com>
2024-02-24 01:06:18 -0600

    Added the ability to compare libraries and extract the empty folders.

--- FLAC_Organizer.py
+++ FLAC_Organizer.py
@@ -6,8 +6,9 @@ from colorama import Fore, Style, Back
 
 # Set file extensions and IO folders.
 ext1 = "flac"
-source_folder = "D:\Musica\Discografias"
-destination_folder = "E:\\Musica\\Discografias"
+source_folder2 = "D:\Musica\Discografias"
+source_folder = "G:\Musica\Discografias"
+destination_folder = "D:\\Musica\\Vacio"
 
 
 def read_audio_file(audio_dir):
@@ -80,13 +81,14 @@ def read_audio_file(audio_dir):
     #if artist.startswith('A') or artist.startswith('3'): destination = ''
     return destination
 
-
 def copy_files(source, destination):
     try:
         if not os.path.exists(destination):
             print("Creating directory - " + destination)
             os.makedirs(destination)
             print("Directory created")
+        # else:
+        #     destination = destination + " QZ"
         shutil.copytree(source, destination, dirs_exist_ok=True)
         print(Fore.GREEN + "Files copied from " + source + " to " + destination + Style.RESET_ALL)
         print("---------------------------------------------------------------------------")
@@ -94,25 +96,60 @@ def copy_files(source, destination):
         print(Fore.RED + "Error copying " + source + " to " + destination + Style.RESET_ALL)
         print("---------------------------------------------------------------------------")
 
+def merge_music_library():
+    for path, dirlist, files in os.walk(source_folder):
+        for file in files:
+            file_ext = file[len(file)-(file[::-1].find('.')):]
+            if file_ext == ext1:
+                destination_path = read_audio_file(path + "\\" + file)
+                if destination_path != '':
+                    with open("errors.txt", "a") as log:
+                        try:
+                            copy_files(path, destination_path)
+                            # Below line will print any print to log file as well.
+                            print("File read to copy: " + file + "\n Fr: " + path + "\n To: " + destination_path, file=log)
+                        except Exception:
+                            traceback.print_exc(file=log)
+                            continue
+                break
+    with open("errors.txt", "a"):
+        print("Copy files finished")
 
-for path, dirlist, files in os.walk(source_folder):
+
+def all_upper(my_list):
+    return [x.upper() for x in my_list]
+
+def empty_folders_check():
+    files = os.listdir(source_folder)
     for file in files:
-        file_ext = file[len(file)-(file[::-1].find('.')):]
-        if file_ext == ext1:
-            destination_path = read_audio_file(path + "\\" + file)
-            if destination_path != '':
-                with open("errors.txt", "a") as log:
-                    try:
-                        copy_files(path, destination_path)
-                        # Below line will print any print to log file as well.
-                        print("File read to copy: " + file + "\n Fr: " + path + "\n To: " + destination_path, file=log)
-                    except Exception:
-                        traceback.print_exc(file=log)
-                        continue
-            break
+        folder = source_folder + "\\" + file
+        #print(folder)
+        if os.path.isdir(folder):
+            if not os.listdir(folder):
+                print(folder)
+
+def empty_folders_extract():
+    paths = []
+
+    main_library = os.listdir(source_folder)
+    second_library = os.listdir(source_folder2)
+
+    for folder in second_library:
+        if folder.upper() not in all_upper(main_library):
+            paths.append(source_folder2 + '\\' + folder)
+    for path in paths:
+        if os.path.isdir(path):
+            if not os.listdir(path):
+                print(path)
+                artist = path[len(path)-(path[::-1].find('\\')):]
+                destination = destination_folder + "\\" + artist
+                if not os.path.exists(destination):
+                    print( artist + " does not exist on: " + destination)
+                    print("Creating directory: " + path)
+                    os.makedirs(destination)
 
 
-
+empty_folders_extract()
 
 
 
